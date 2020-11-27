ef0224d8b2489d941165bf7783fbad7039554376
Rahat <djrayhan8@gmail.com>
2020-11-27 20:14:02 +0600

    completed the app to funcion properly. now just some minor edits required

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ let mongodb = require('mongodb')
 let app = express()
 let db
 
+app.use(express.static('public'))
+
 
 let connectionString = 'mongodb+srv://rahat47:RahatRock0048@cluster0.og4lf.mongodb.net/TodoApp?retryWrites=true&w=majority'
 mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function (err, client) {
@@ -11,7 +13,7 @@ mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: tr
 })
 
 
-
+app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', function (req, res) {
@@ -29,35 +31,49 @@ app.get('/', function (req, res) {
         <h1 class="display-4 text-center py-1">To-Do App!</h1>
         
         <div class="jumbotron p-3 shadow-sm">
-          <form action="/create-item", method="POST">
+          <form action="/create-item", method="POST" id="create-form">
             <div class="d-flex align-items-center">
-              <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+              <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
               <button class="btn btn-primary">Add New Item</button>
             </div>
           </form>
         </div>
         
-        <ul class="list-group pb-5">
+        <ul id="item-list" class="list-group pb-5">
           ${items.map(function (item) {
               return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
                     <span class="item-text">${item.text}</span>
                             <div>
-                                <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-                                <button class="delete-me btn btn-danger btn-sm">Delete</button>
+                                <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                                <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
                             </div>
                          </li>`
           }).join('')}
         </ul>
         
       </div>
-      
+      <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+      <script src="/browser.js"></script>
     </body>
     </html>`)
     })
     
 })
 app.post('/create-item', function (req, res) {
-    db.collection('items').insertOne({text: req.body.item}, function() {
-        res.redirect('/')
+    db.collection('items').insertOne({text: req.body.text}, function(err, info) {
+        res.json(info.ops[0])
     })
+})
+
+app.post('/update-item', function(req, res) {
+  db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: req.body.text}}, function() {
+    res.send("Success")
+  })
+  
+})
+
+app.post('/delete-item', function(req, res) {
+  db.collection('items').deleteOne({_id: new mongodb.ObjectId(req.body.id)}, function() {
+    res.send("Success")
+  })
 })
